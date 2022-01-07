067844f1c52f53a4352cb44258f05dfe9441b555
ibrizuela-eb <ibrizuela@eventbrite.com>
2022-01-07 16:46:47 -0300

    Add conjured item

--- python/common/__init__.py
+++ python/common/__init__.py
@@ -1,4 +1,4 @@
 CHEESE_ITEM_NAMES = ["Aged Brie"]
-CONJURED_ITEM_NAMES = ["Conjured Mana Cake"]
+CONJURED_ITEM_NAMES = ["Conjured Mana Cake", "+5 Dexterity Vest"]
 SULFURAS_ITEM_NAMES = ["Sulfuras, Hand of Ragnaros"]
 TICKET_ITEM_NAMES = ["Backstage passes to a TAFKAL80ETC concert"]
